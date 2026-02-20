67247039d4f9a214bbd623f6cbf875c92329ce63
“Andrew <xhinker@hotmail.com>
2026-02-20 13:39:38 -0800

    add minicpm sample

---
+++ ai_models_eval/llm_models/minicpm-o/readme.md
@@ -0,0 +1,3 @@
+# miniCPM-o note
+
+This is actually a combination of lots of models into one. 
--- ai_models_eval/llm_models/minicpm-o/sample_code.py
+++ ai_models_eval/llm_models/minicpm-o/sample_code.py
@@ -1,12 +1,15 @@
 #%%
 import torch
-from transformers import AutoModel
+from transformers import AutoModel,AutoTokenizer
+import librosa
 
 # Load omni model (default: init_vision=True, init_audio=True, init_tts=True)
 # For vision-only model: set init_audio=False and init_tts=False
 # For audio-only model: set init_vision=False
+
+model_path = "/home/andrewzhu/storage_1t_1/az_git_folder/az_samples/ai_models_eval/llm_models/minicpm-o/models/llm_hf_models/openbmb/MiniCPM-o-4_5_main"
 model = AutoModel.from_pretrained(
-    "/home/andrewzhu/storage_1t_1/az_git_folder/az_samples/ai_models_eval/llm_models/minicpm-o/models/llm_hf_models/openbmb/MiniCPM-o-4_5_main",
+    model_path,
     trust_remote_code=True,
     attn_implementation="sdpa", # sdpa or flash_attention_2
     torch_dtype=torch.bfloat16,
@@ -15,14 +18,20 @@ model = AutoModel.from_pretrained(
     init_tts=True,
 )
 model.eval().cuda()
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
 #%% use its tts 
-import librosa
+
 model.init_tts()
+# model.tts.float()
 
 #%%
 ref_audio_path = "/home/andrewzhu/storage_1t_1/az_git_folder/az_samples/ai_models_eval/voice_models/cosyvoice2/samples/Zh_7_prompt.wav"
+# ref_audio_path = "/home/andrewzhu/storage_1t_1/az_git_folder/az_samples/ai_models_eval/voice_models/qwen3-tts/outputs/output_custom_voice_1.wav"
+# ref_audio_path = "/home/andrewzhu/storage_1t_1/az_git_folder/az_samples/ai_models_eval/voice_models/qwen3-tts/outputs/output_voice_design.wav"
+# ref_audio_path = "/home/andrewzhu/storage_1t_1/az_git_folder/azcode/az_projects/ebook2audio/voice_samples/martian_audio_sample/martian_audio_sample_1.wav"
 ref_audio, _ = librosa.load(ref_audio_path, sr=16000, mono=True)
+print(ref_audio)
 sys_msg = {"role": "system", "content": [
   "模仿音频样本的音色并生成新的内容。",
   ref_audio,
@@ -34,19 +43,34 @@ sys_msg = {"role": "system", "content": [
 user_msg = {
   "role": "user",
   "content": [
-    "请朗读以下内容。" + " " + "你好，欢迎来到艾米说科幻，我是艾米。"
+    "请朗读以下内容。  "+"""
+    我叫李若狐，刚从学校毕业不久
+    """
   ]
 }
 
 msgs = [sys_msg, user_msg]
+# tts_result = model.chat(
+#     msgs=msgs,
+#     image=None,
+#     tokenizer=tokenizer,
+#     sampling=True,
+#     temperature=0.3,
+#     max_new_tokens=4096,
+#     use_tts_template=True,
+#     generate_audio=True,
+#     output_audio_path='text2speech_output.wav',
+#     ref_audio=ref_audio
+# )
 res = model.chat(
-    msgs=msgs,
-    do_sample=True,
-    max_new_tokens=512,
-    use_tts_template=True,
-    generate_audio=True,
-    temperature=0.1,
-    output_audio_path="result_voice_cloning.wav",
+    msgs                = msgs,
+    do_sample           = True,
+    max_new_tokens      = 1024,
+    use_tts_template    = True,
+    generate_audio      = True,
+    temperature         = 0.1,
+    output_audio_path   = "result_voice_cloning.wav",
+    ref_audio           = ref_audio
 )
 print(res)
 
